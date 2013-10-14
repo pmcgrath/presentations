f999e832d2ea831887aa805e5d7027f704033a98
pmcgrath <pmcgrath@gmail.com>
2013-10-15 00:39:37 +0100

    Added message publication loop

--- producer.cs
+++ producer.cs
@@ -1,26 +1,46 @@
 // See http://www.rabbitmq.com/dotnet.html for driver lib
 using RabbitMQ.Client;
 using System;
+using System.Text;
+
 
 class App
 {
 	static void Main()
 	{
+		var _exchangeName = "demo_exchange";
+		var _queueName = "demo_queue";
+	
 		var _connectionFactory = new ConnectionFactory();
 		var _connection = _connectionFactory.CreateConnection();
 		var _channel = _connection.CreateModel();
 
 		Console.WriteLine("Hit enter to declare exchange");
 		Console.ReadLine();
-		_channel.ExchangeDeclare("demo_exchange", ExchangeType.Fanout); 
+		_channel.ExchangeDeclare(_exchangeName, ExchangeType.Fanout); 
 		
 		Console.WriteLine("Hit enter to declare queue");
 		Console.ReadLine();	
-		_channel.QueueDeclare("demo_queue", false, false, false, null); 
+		_channel.QueueDeclare(_queueName, false, false, false, null); 
 		
 		Console.WriteLine("Hit enter to bind queue");
 		Console.ReadLine();
-		_channel.QueueBind("demo_queue", "demo_exchange", string.Empty, null);
+		_channel.QueueBind(_queueName, _exchangeName, string.Empty, null);
+		
+
+		do
+		{
+			var _messageProperties = _channel.CreateBasicProperties();
+			_messageProperties.ContentType = "text/plain";
+			_messageProperties.Type = "amessage";
+			var _messageBodyContent = string.Format("Message created @ {0}", DateTime.Now);
+			var _messageBodyData = Encoding.UTF8.GetBytes(_messageBodyContent);
+			_channel.BasicPublish(_exchangeName, string.Empty, _messageProperties, _messageBodyData);
+			
+			Console.WriteLine("Just published message with content [{0}]", _messageBodyContent);
+			Console.WriteLine("Hit enter to publish another message, use x to stop publishing");
+		}
+		while (Console.ReadLine() != "x");
 
 		Console.WriteLine("Hit enter to close channel");
 		Console.ReadLine();
