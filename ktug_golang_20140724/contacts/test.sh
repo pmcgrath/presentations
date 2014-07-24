00824ae17f62b1bd88fb30908bbfd09a1dfef782
pmcgrath <pmcgrath@gmail.com>
2014-07-24 17:55:24 +0100

    Added test cases

--- ktug_golang_20140724/contacts/test.sh
+++ ktug_golang_20140724/contacts/test.sh
@@ -1,3 +1,31 @@
-curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Tom Joe", "surname": "a" }'
+echo *** Root
+curl http://localhost:8080/ -v 
 
+echo *** Contacts
+curl http://localhost:8080/contacts -v 
+echo *** Add
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Tom Joe", "surname": "a" }'
 curl http://localhost:8080/contacts/tom%20joe%20a -v
+echo *** Update 
+curl http://localhost:8080/contacts/tom%20joe%20a -v -XPUT -d '{ "id": "tom joe a", "forename": "Tom Joe", "surname": "ALTERED SURNAME" }'
+curl http://localhost:8080/contacts/tom%20joe%20a -v
+echo *** Delete
+curl http://localhost:8080/contacts/tom%20joe%20a -v -XDELETE
+curl http://localhost:8080/contacts/tom%20joe%20a -v
+
+echo *** Adding a bunch of contacts qith same name - all willbe given different ids - see location header value
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v -XPOST -d '{ "forename": "Ted", "surname": "Toe" }'
+curl http://localhost:8080/contacts/ -v
+
+
+
