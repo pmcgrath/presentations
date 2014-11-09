d73543e3a17eceddda560e9d3f11f5f05aaa3947
pmcgrath <pmcgrath@gmail.com>
2014-11-09 10:28:30 +0000

    Added signal traps to shell scripts

--- ktug_docker_20141106/demo2/app.sh
+++ ktug_docker_20141106/demo2/app.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+function cleanUp {
+       echo ="$(date) EXITING"
+       exit 0
+}
+
+trap cleanUp SIGINT SIGHUP SIGTERM
+
 # Cater for running in a container and outside a container, could have used http://stackoverflow.com/questions/23513045/how-to-check-if-a-process-is-running-inside-docker-container
 logDirectoryPath=log; [ -d /log ] && logDirectoryPath=/log
 
 seq=1
-while [ true ]
+while true
 do
 	message="$(date) on $(hostname) seq $seq"
 	logFilePath="$logDirectoryPath/$(date +"%Y%m%d_%H%M").log"
