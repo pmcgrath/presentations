e01fc7db0b3c62a4cdfdff63e59a6abb345d8c33
pmcgrath <pmcgrath@gmail.com>
2016-10-15 10:28:02 +0100

    Fixed security group rules issue

--- ktug_aws_20161012/infra/main.tf
+++ ktug_aws_20161012/infra/main.tf
@@ -1,5 +1,5 @@
 /*
-	Going to keep all in a single file as not much here, would normally break into varibales.tf, ouputs.tf etc.
+	Going to keep all in a single file as not much here, would normally break into variables.tf, ouputs.tf etc.
 	This is not json, this is HCL, see https://github.com/hashicorp/hcl
 	See https://www.terraform.io/docs/providers/aws/
 	Going to order alphabetically
@@ -7,15 +7,15 @@
 
 	Will create the following
 		A single VPC
-		2 subnets in the first AZ
+		A pair of subnets in the the first N subnets where N is the var.availability_zone_count value
 			- Public will host the NAT gateway and any public EC2 instances
 			- Private - will host EC2 instances that do not have public IPs, will go via the NAT Gateway to access the internet
-		1 Elastic IP
+		N Elastic IPs where N is the var.availability_zone_count value
 		1 internet gateway for the VPC
-		1 NAT gateway with the EIP in the public subnet
-		2 route tables
+		N NAT gateway with the EIP in the public subnets where N is the var.availability_zone_count
+		N + 1 route tables
 			- Public that has a 0.0.0.0/0 route via the internet gateway
-			- Private that has a 0.0.0.0/0 route via the NAT gateway gateway
+			- N Private that has a 0.0.0.0/0 route via the NAT gateways where N is the var.availability_zone_count value
 		3 security groups
 			- App
 				- 22 from Bastion SG
@@ -31,6 +31,7 @@
 			- WebFront
 				- 22 from Bastion SG
 				- 80 from internet
+				- 443 from internet
 				- 5000 to App SG
 				- 80 to 0.0.0.0/0
 				- 443 to 0.0.0.0/0
@@ -42,8 +43,16 @@
 data "aws_availability_zones" "available" {}
 
 # Outputs
-output "nat_gateway_public_ip" {
-  value = "${aws_eip.nat.public_ip}"
+output "nat_gateway_public_ips" {
+  value = "${list(aws_eip.nat_gateway.*.public_ip)}"
+}
+
+output "private_subnet_ids" {
+  value = "${list(aws_subnet.private.*.id)}"
+}
+
+output "public_subnet_ids" {
+  value = "${list(aws_subnet.public.*.id)}"
 }
 
 # Configure the AWS Provider - Will only set the region - do not want to commit access keys to SCM - that would be expensive
@@ -52,7 +61,9 @@ provider "aws" {
 }
 
 # See https://www.terraform.io/docs/providers/aws/r/eip.html
-resource "aws_eip" "nat" {}
+resource "aws_eip" "nat_gateway" {
+  count = "${var.availability_zone_count}"
+}
 
 # See https://www.terraform.io/docs/providers/aws/r/internet_gateway.html
 resource "aws_internet_gateway" "main" {
@@ -65,8 +76,10 @@ resource "aws_internet_gateway" "main" {
 
 # See https://www.terraform.io/docs/providers/aws/r/nat_gateway.html
 resource "aws_nat_gateway" "main" {
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${aws_subnet.public.id}"
+  count         = "${var.availability_zone_count}"
+  allocation_id = "${aws_eip.nat_gateway.*.id[count.index]}"
+  subnet_id     = "${aws_subnet.public.*.id[count.index]}"
+  depends_on    = ["aws_internet_gateway.main"]
 }
 
 # See https://www.terraform.io/docs/providers/aws/r/route.html
@@ -77,9 +90,10 @@ resource "aws_route" "internet" {
 }
 
 resource "aws_route" "nat" {
-  route_table_id         = "${aws_route_table.nat.id}"
+  count                  = "${var.availability_zone_count}"
+  route_table_id         = "${aws_route_table.nat.*.id[count.index]}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.main.id}"
+  nat_gateway_id         = "${aws_nat_gateway.main.*.id[count.index]}"
 }
 
 # See https://www.terraform.io/docs/providers/aws/r/route.html
@@ -92,10 +106,11 @@ resource "aws_route_table" "internet" {
 }
 
 resource "aws_route_table" "nat" {
+  count  = "${var.availability_zone_count}"
   vpc_id = "${aws_vpc.main.id}"
 
   tags {
-    Name = "${var.vpc_name}-nat"
+    Name = "${var.vpc_name}-nat-${count.index}"
   }
 }
 
@@ -104,101 +119,38 @@ resource "aws_security_group" "app" {
   description = "Apps"
   vpc_id      = "${aws_vpc.main.id}"
   name        = "${var.vpc_name}-app"
-
-  egress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "bastion" {
   description = "Bastions"
   vpc_id      = "${aws_vpc.main.id}"
   name        = "${var.vpc_name}-bastion"
-
-  egress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = ["${var.vpc_cidr_block}"]
-  }
-
-  egress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = ["${var.bastion_ssh_access_cidr_block}"]
-  }
 }
 
 resource "aws_security_group" "webfront" {
   description = "Web frontends"
   vpc_id      = "${aws_vpc.main.id}"
   name        = "${var.vpc_name}-webfront"
-
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 # See https://www.terraform.io/docs/providers/aws/r/security_group_rule.html
-# Cannot inline some of these due to cyclic issues, so need to extract
-resource "aws_security_group_rule" "app_ingress_bastion_22" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.app.id}"
-  source_security_group_id = "${aws_security_group.bastion.id}"
+# Cannot inline some of these due to cyclic issues, also warns about mixing inline and external rules, so need to extract all
+resource "aws_security_group_rule" "app_egress_all_80" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.app.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "app_egress_all_443" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.app.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "app_ingress_webfront_5000" {
@@ -208,6 +160,60 @@ resource "aws_security_group_rule" "app_ingress_webfront_5000" {
   protocol                 = "tcp"
   security_group_id        = "${aws_security_group.app.id}"
   source_security_group_id = "${aws_security_group.webfront.id}"
+}
+
+resource "aws_security_group_rule" "bastion_egress_vpc_22" {
+  type              = "egress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.bastion.id}"
+  cidr_blocks       = ["${var.vpc_cidr_block}"]
+}
+
+resource "aws_security_group_rule" "bastion_egress_all_80" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.bastion.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "bastion_egress_all_443" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.bastion.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "bastion_ingress_secure_22" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.bastion.id}"
+  cidr_blocks       = ["${var.bastion_ssh_access_cidr_block}"]
+}
+
+resource "aws_security_group_rule" "webfront_egress_all_80" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.webfront.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "webfront_egress_all_443" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.webfront.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "webfront_egress_app_5000" {
@@ -228,24 +234,44 @@ resource "aws_security_group_rule" "webfront_ingress_bastion_22" {
   source_security_group_id = "${aws_security_group.bastion.id}"
 }
 
+resource "aws_security_group_rule" "webfront_ingress_all_80" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.webfront.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "webfront_inress_all_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.webfront.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 # See https://www.terraform.io/docs/providers/aws/r/subnet.html
 resource "aws_subnet" "private" {
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-  cidr_block        = "${var.private_subnet_cidr_block}"
+  count             = "${var.availability_zone_count}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  cidr_block        = "${var.private_subnet_cidr_blocks[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
 
   tags {
-    Name = "${var.vpc_name}-private"
+    Name = "${var.vpc_name}-private-${count.index}"
   }
 }
 
 resource "aws_subnet" "public" {
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-  cidr_block        = "${var.public_subnet_cidr_block}"
+  count             = "${var.availability_zone_count}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  cidr_block        = "${var.public_subnet_cidr_blocks[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
 
   tags {
-    Name = "${var.vpc_name}-public"
+    Name = "${var.vpc_name}-public-${count.index}"
   }
 }
 
@@ -263,14 +289,20 @@ variable "aws_region" {
   default = "eu-west-1"
 }
 
-variable "bastion_ssh_access_cidr_block" {}
-
-variable "private_subnet_cidr_block" {
-  default = "10.0.1.0/24"
+variable "availability_zone_count" {
+  default = 2
 }
 
-variable "public_subnet_cidr_block" {
-  default = "10.0.0.0/24"
+variable "bastion_ssh_access_cidr_block" {
+  description = "CIDR block that is allowed access the bastions - Will need to supply - Your IP address with a /32"
+}
+
+variable "private_subnet_cidr_blocks" {
+  default = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24", "10/0.14.0/24"]
+}
+
+variable "public_subnet_cidr_blocks" {
+  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10/0.4.0/24"]
 }
 
 variable "vpc_cidr_block" {
