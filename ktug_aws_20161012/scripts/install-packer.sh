7620722a51817579762c53fe3ea21dcd8d8dcbbe
pmcgrath <pmcgrath@gmail.com>
2016-10-31 21:19:23 +0000

    Upgraded packer 0.10.2 to 0.11.0

--- ktug_aws_20161012/scripts/install-packer.sh
+++ ktug_aws_20161012/scripts/install-packer.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt-get install -y unzip
 
 # Alter version for the current version and grab binary, extracting into /usr/local/bin - Is a single binary
-version=0.10.2
+version=0.11.0
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip
 sudo unzip -o /tmp/packer.zip -d /usr/local/bin
 
