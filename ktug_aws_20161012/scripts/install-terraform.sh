bdc0471c3bbc7253f9ff6e525fb7ed1720f1d7c4
pmcgrath <pmcgrath@gmail.com>
2016-11-02 19:53:10 +0000

    Upgraded terrafrom to v0.7.8

--- ktug_aws_20161012/scripts/install-terraform.sh
+++ ktug_aws_20161012/scripts/install-terraform.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt-get install -y unzip
 
 # Alter version for the current version and grab binary, extracting into /usr/local/bin - Is a single binary since 0.7.0
-version=0.7.6
+version=0.7.8
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip
 sudo unzip -o /tmp/terraform.zip -d /usr/local/bin
 
